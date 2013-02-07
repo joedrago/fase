b960b5d02b63081e557aa850da97e4b75d972322
Joe Drago <joedrago@gmail.com>
2013-02-06 16:53:07 -0800

    Added -o ("only run once") and -t X-Y (animations are randomly between X and Y seconds apart)

--- fase.c
+++ fase.c
@@ -3,6 +3,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
@@ -24,15 +25,13 @@ static const faseMovement sMovesDWIFace[] = {
     TELEPORT  (ANCHOR_LB, RANDOM0, -250),
     MOVE_Y    (ANCHOR_LB, 0, 2000 ms),
     WAIT      (ANCHOR_LB, 4000 ms),
-    MOVE_Y    (ANCHOR_LB, -250, 1000 ms),
-    WAIT      (ANCHOR_LB, 2000 ms)
+    MOVE_Y    (ANCHOR_LB, -250, 1000 ms)
 };
 static const faseMovement sMovesDWIGlasses[] = {
     TELEPORT  (ANCHOR_LT, RANDOM1, -250),
     MOVE      (ANCHOR_LB, RANDOM0, 0, 2000 ms),
     WAIT      (ANCHOR_LB, 4000 ms),
-    MOVE_Y    (ANCHOR_LB, -250, 1000 ms),
-    WAIT      (ANCHOR_LB, 2000 ms)
+    MOVE_Y    (ANCHOR_LB, -250, 1000 ms)
 };
 static const faseMovement sMovesDWIText[] = {
     OFFSCREEN (2000 ms),
@@ -45,27 +44,60 @@ static const faseMovement sMovesPeekBottom[] = {
     TELEPORT  (ANCHOR_LB, RANDOM0, -250),
     MOVE_Y    (ANCHOR_LB, -150, 2000 ms),
     WAIT      (ANCHOR_LB, 4000 ms),
-    MOVE_Y    (ANCHOR_LB, -250, 1000 ms),
-    WAIT      (ANCHOR_LB, 2000 ms)
+    MOVE_Y    (ANCHOR_LB, -250, 1000 ms)
 };
 
 static const faseMovement sMovesPeekTop[] = {
     TELEPORT  (ANCHOR_LT, RANDOM0, -250),
     MOVE_Y    (ANCHOR_LT, -150, 2000 ms),
     WAIT      (ANCHOR_LT, 4000 ms),
-    MOVE_Y    (ANCHOR_LT, -250, 1000 ms),
-    WAIT      (ANCHOR_LT, 2000 ms)
+    MOVE_Y    (ANCHOR_LT, -250, 1000 ms)
 };
 
 static const faseMovement sMovesPeekBottomLeft[] = {
     TELEPORT  (ANCHOR_LB, -310, -265),
     MOVE      (ANCHOR_LB, -120, -120, 2000 ms),
     WAIT      (ANCHOR_LB, 4000 ms),
-    MOVE      (ANCHOR_LB, -310, -265, 1000 ms),
-    WAIT      (ANCHOR_LB, 2000 ms)
+    MOVE      (ANCHOR_LB, -310, -265, 1000 ms)
 };
 
 #undef ms
+
+#ifdef _DEBUG
+#define DEBUGSLEEP(FMT, ...) { char fff[1024]; sprintf_s(fff, 1024, FMT, __VA_ARGS__); OutputDebugString(fff); }
+#else
+#define DEBUGSLEEP(FMT, ...)
+#endif
+
+static void parseTimeDelay(const char *s, int *timeDelayMin, int *timeDelayMax)
+{
+    char temp[64];
+    char *minStr = temp;
+    char *maxStr;
+    int v;
+    strncpy_s(temp, 64, s, 63);
+    temp[63] = 0;
+
+    maxStr = strchr(temp, '-');
+    if(maxStr)
+    {
+        *maxStr = 0;
+        ++maxStr;
+    }
+    else
+    {
+        maxStr = minStr;
+    }
+
+    // (one second - one week) is a reasonable clamp
+    v = atoi(minStr);
+    *timeDelayMin = (v > 1) ? v : 1;
+    v = atoi(maxStr);
+    *timeDelayMax = (v < 604800) ? v : 604800;
+
+    if(*timeDelayMax < *timeDelayMin)
+        *timeDelayMax = *timeDelayMin;
+}
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
@@ -74,9 +106,35 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
     faseAnimation *anims[4];
     int animCount = 0;
     int curAnim = 0;
+    int i;
+    int onlyOnce = 0;
+    int timeDelayMin = 1;
+    int timeDelayMax = 10;
+    int sleeping = 1;
+    int sleepStart, sleepLength;
 
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+
+    for(i = 1; i < __argc; ++i)
+    {
+        if(__argv[i][0] == '-')
+        {
+            switch(__argv[i][1])
+            {
+            case 'o':
+                onlyOnce = 1;
+                break;
+            case 't':
+                ++i;
+                if(i < __argc)
+                {
+                    parseTimeDelay(__argv[i], &timeDelayMin, &timeDelayMax);
+                }
+                break;
+            };
+        }
+    }
 
     faseSpriteStartup(hInstance);
 
@@ -118,7 +176,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
         anims[animCount++] = anim;
     }
 
-    curAnim = rand() % animCount;
+    sleepStart = (int)GetTickCount();
+    sleepLength = timeDelayMin;
+    if(timeDelayMax != timeDelayMin)
+        sleepLength += rand() % (timeDelayMax - timeDelayMin);
+    DEBUGSLEEP("sleeping for %d seconds\n", sleepLength);
 
     while(1)
     {
@@ -136,16 +198,38 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
         }
         else
         {
-            if(!faseAnimationThink(anims[curAnim], (int)GetTickCount()))
+            if(sleeping)
             {
-                int i;
-                for(i = 0; i < animCount; ++i)
+                int now = (int)GetTickCount();
+                if((now - sleepStart) > (sleepLength * 1000))
                 {
-                    faseAnimationHide(anims[i]);
+                    curAnim = rand() % animCount;
+                    faseAnimationReset(anims[curAnim], (int)GetTickCount());
+                    sleeping = 0;
+                    DEBUGSLEEP("running animation %d\n", curAnim);
                 }
+            }
+            else
+            {
+                if(!faseAnimationThink(anims[curAnim], (int)GetTickCount()))
+                {
+                    if(onlyOnce)
+                    {
+                        PostQuitMessage(0);
+                    }
 
-                curAnim = rand() % animCount;
-                faseAnimationReset(anims[curAnim], (int)GetTickCount());
+                    for(i = 0; i < animCount; ++i)
+                    {
+                        faseAnimationHide(anims[i]);
+                    }
+
+                    sleepLength = timeDelayMin;
+                    if(timeDelayMax != timeDelayMin)
+                        sleepLength += rand() % (timeDelayMax - timeDelayMin);
+                    sleepStart = (int)GetTickCount();
+                    sleeping = 1;
+                    DEBUGSLEEP("sleeping for %d seconds\n", sleepLength);
+                }
             }
             Sleep(10);
         }
